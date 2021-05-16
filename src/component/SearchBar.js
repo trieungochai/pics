c78baf697294ade1e52ccfe6fe625c71d47fefb0
hai.trieu <trieungochai.dev@gmail.com>
2021-05-16 14:10:48 +0700

    Adding controlled elements

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -1,17 +1,24 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
-           <label>Image Search</label>
-            <input type="text" />
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(event) => this.setState({ term: event.target.value })}
+            />
           </div>
         </form>
       </div>
     );
   }
 }
+
 export default SearchBar;
