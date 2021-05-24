2c029ecc8df776e4fb240d1f9b8621436bf19af3
hai.trieu <trieungochai.dev@gmail.com>
2021-05-24 11:18:36 +0700

    Fetching data

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import axios from "axios";
 
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit = (term) => {
-    console.log(term);
-  };
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID zptHMdAEwTVsK56nWVpNEbm9rAM2U2XoiGXryhc4lQ8",
+      },
+    });
+  }
 
   render() {
     return (
